d1286980d9cfc0959d8c4071396f21056f8d4864
Eric Kolker <e.kolker@gmail.com>
2014-04-23 18:27:01 -0400

    shuffling comments around in servo.js, calling configureservo in sweep.js

--- examples/sweep.js
+++ examples/sweep.js
@@ -15,5 +15,13 @@ var loop = function() {
 }
 
 servoController.on('ready', function () {
-  loop();
+  //  Set the minimum and maximum duty cycle for servo 1.
+  //
+  //  If the servo doesn't move to its full extent or stalls out
+  //  and gets hot, try tuning these values (0.05 and 0.12). 
+  //  Moving them towards each other = less movement range
+  //  Moving them apart = more range, more likely to stall and burn out
+  servoController.configureServo(1, 0.05, 0.12, function () {
+    loop();
+  });
 });
