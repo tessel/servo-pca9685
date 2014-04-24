d1f0f5ea8334b405ed16876700420d8e0f2c79e2
Frijol <ifoundthemeaningoflife@gmail.com>
2014-04-24 17:03:29 -0400

    delinted servo sweep example

--- examples/sweep.js
+++ examples/sweep.js
@@ -11,14 +11,14 @@ var loop = function() {
         setTimeout(loop, 500);
       });
     }, 500);
-  })
-}
+  });
+};
 
 servoController.on('ready', function () {
   //  Set the minimum and maximum duty cycle for servo 1.
   //
   //  If the servo doesn't move to its full extent or stalls out
-  //  and gets hot, try tuning these values (0.05 and 0.12). 
+  //  and gets hot, try tuning these values (0.05 and 0.12).
   //  Moving them towards each other = less movement range
   //  Moving them apart = more range, more likely to stall and burn out
   servoController.configureServo(1, 0.05, 0.12, function () {
